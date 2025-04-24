243685386b0f56305e21b8ade739baa29e64ecbd
ParkJin0814 <114732394+ParkJin0814@users.noreply.github.com>
2025-04-24 15:32:38 +0900

    Lv5 캡슐화 수정

---
+++ src/kiosk/lv5/Kiosk.java
@@ -0,0 +1,66 @@
+package kiosk.lv5;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Kiosk {
+    private List<Menu> menuList;
+    private Scanner sc = new Scanner(System.in);
+
+    Kiosk(List<Menu> menuList){
+        this.menuList = menuList;
+    }
+
+    public void start(){
+        selectMainMenu();
+    }
+
+    private int inputNumber(int length){
+        while (true){
+            try {
+                System.out.println("-----------------------------------------");
+                System.out.print("번호를 입력해주세요 : ");
+                String inputText = sc.nextLine();
+                int index = Integer.parseInt(inputText) -1;
+                // 메뉴아이템으로 넘어가기
+                if(index < -1 || index > length - 1){
+                    throw new Exception();
+                }
+                System.out.println("-----------------------------------------");
+                return index;
+            } catch (Exception e){
+                System.out.println("제시된 숫자중에서 입력해주세요");
+            }
+        }
+    }
+
+    private void printMainMenuList(){
+        System.out.println("[ Main MENU ]");
+        int menuNumber = 1;
+        for(Menu m : menuList){
+            System.out.printf("%d. %s\n", menuNumber, m.getTitleName());
+            menuNumber++;
+        }
+        System.out.println("0. 종료      | 종료");
+    }
+
+    private void selectMainMenu(){
+        printMainMenuList();
+
+        int index = inputNumber(menuList.size());
+        if(index != -1) {
+            selectItem(menuList.get(index));
+        }
+    }
+
+    private void selectItem(Menu menu){
+        menu.printMenuItemList();
+
+        int index = inputNumber(menu.getMenuItemList().size());
+        // 선택된 메뉴 출력
+        if(index != -1) {
+            menu.printSelectMenu(index);
+        }
+        selectMainMenu();
+    }
+}
---
+++ src/kiosk/lv5/Menu.java
@@ -0,0 +1,40 @@
+package kiosk.lv5;
+
+import java.util.List;
+
+public class Menu {
+    private String titleName;
+    private List<MenuItem> menuItemList;
+
+    Menu(String titleName, List<MenuItem> menuItemList){
+        this.titleName = titleName;
+        this.menuItemList = menuItemList;
+    }
+
+    public void printMenuItemList(){
+        System.out.printf("[ %s ]\n", titleName);
+        int menuNumber = 1;
+        for(MenuItem m : menuItemList){
+            System.out.printf("%d. %s | ￦ %.1f | %s \n", menuNumber, m.getName(), m.getPrice(), m.getDescription());
+            menuNumber++;
+        }
+        System.out.println("0. 뒤로가기");
+    }
+
+    public void printSelectMenu(int index){
+        System.out.println("-----------------------------------------");
+        System.out.printf("%s | ￦ %.1f | %s \n",
+                menuItemList.get(index).getName(),
+                menuItemList.get(index).getPrice(),
+                menuItemList.get(index).getDescription());
+        System.out.println("-----------------------------------------");
+    }
+
+    public String getTitleName(){
+        return titleName;
+    }
+
+    public List<MenuItem> getMenuItemList(){
+        return menuItemList;
+    }
+}
